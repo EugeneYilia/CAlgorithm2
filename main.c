4d6ca07c5f2c1db242e857abaca3fdb7bda1170c
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-09 13:16:07 +0800

    change a little

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int numbers[10];
-    printf("请输入十个数字\n");
-    for (int i = 0; i < 10; i++) {
+    printf("请输入想要的数组的大小\n");
+    int size;
+    scanf("%d", &size);
+    int numbers[size];
+    printf("请输入%d个数字\n", size);
+    for (int i = 0; i < size; i++) {
         scanf("%d", &numbers[i]);
     }
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9 - i; j++) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
             if (numbers[j] > numbers[j + 1]) {
                 int tempNumber = numbers[j + 1];
                 numbers[j + 1] = numbers[j];
@@ -15,7 +18,7 @@ int main() {
             }
         }
     }
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < size; i++) {
         printf("%d\n", numbers[i]);
     }
     return 0;
