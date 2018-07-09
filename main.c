4465617ad6a7e6931a9f68b2aa74305e35f34156
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-09 13:12:03 +0800

    finish algorithm2

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int numbers[10];
+    printf("请输入十个数字\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9 - i; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                int tempNumber = numbers[j + 1];
+                numbers[j + 1] = numbers[j];
+                numbers[j] = tempNumber;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", numbers[i]);
+    }
     return 0;
 }
