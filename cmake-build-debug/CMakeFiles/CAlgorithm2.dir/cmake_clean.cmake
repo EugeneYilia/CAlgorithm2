ab99ab8d2eb6011fbd9dd4106ad09baf7489c3f3
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-09 13:06:54 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/CAlgorithm2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CAlgorithm2.dir/main.c.obj"
+  "CAlgorithm2.pdb"
+  "CAlgorithm2.exe"
+  "CAlgorithm2.exe.manifest"
+  "libCAlgorithm2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CAlgorithm2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
